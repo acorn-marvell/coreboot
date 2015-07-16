b362b37fb4a13678e41dfcd839e9d8edf863a40b
Ruilin Hao <rlhao@marvell.com>
2015-07-16 13:19:13 -0700

    update configs for cyclone

--- src/mainboard/marvell/cyclone/mainboard.c
+++ src/mainboard/marvell/cyclone/mainboard.c
@@ -47,17 +47,10 @@ static void mainboard_enable(device_t dev)
 }
 
 struct chip_operations mainboard_ops = {
-	.name	= "acorn",
+	.name	= "cyclone",
 	.enable_dev = mainboard_enable,
 };
 
 void lb_board(struct lb_header *header)
 {
-	struct lb_range *dma;
-
-	dma = (struct lb_range *)lb_new_record(header);
-	dma->tag = LB_TAB_DMA;
-	dma->size = sizeof(*dma);
-	dma->range_start = CONFIG_DRAM_DMA_START;
-	dma->range_size = CONFIG_DRAM_DMA_SIZE;
 }
